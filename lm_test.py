cad45119378e3ff0b34f0bc669e81947b12e7b1d
Edgar Ortiz <ed.ortizm@gmail.com>
2020-04-02 17:24:38 -0300

    ordering the directories

--- lm_test.py
+++
@@ -1,9 +0,0 @@
-import sys
-from lm00 import *
-n = int(sys.argv[1])
-x = float(sys.argv[2])
-y = float(sys.argv[3])
-p0 = np.array([x,y])
-f = Function(n=n)
-p = lm(p0,f)
-print("the optimal point is:", p)
